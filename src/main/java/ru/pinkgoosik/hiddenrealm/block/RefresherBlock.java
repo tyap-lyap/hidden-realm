226b03380f8400710b5931bcfbcef593eb74417d
PinkGoosik <MCGycuk@yandex.ru>
2024-10-06 22:46:21 +0500

    add missing recipes

--- src/main/java/ru/pinkgoosik/hiddenrealm/block/RefresherBlock.java
+++ src/main/java/ru/pinkgoosik/hiddenrealm/block/RefresherBlock.java
@@ -68,7 +68,10 @@ public class RefresherBlock extends Block {
 
 	@Override
 	public BlockState getPlacementState(ItemPlacementContext ctx) {
-		return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+		if(ctx.getWorld().getBlockState(ctx.getBlockPos().up()).isAir()) {
+			return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+		}
+		return null;
 	}
 
 	@Override
